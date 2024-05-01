030979a3289211eeb9509914b76b6241d4a6c4bc
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-05-01 13:36:01 +0530

    gggggggggggg

--- client/src/components/02-03/context/AuthContext.jsx
+++ client/src/components/02-03/context/AuthContext.jsx
@@ -1,43 +1,50 @@
+import axios from "axios";
 import { createContext, useEffect, useReducer } from "react";
 
 export const AuthContext = createContext();
-function Reducer(state,action){
-    switch(action.type){
-        case "LOGIN":
-            return{...state, user:action.payload}
-        case "LOGOUT":
-            return{...state, user:null}
-            default:
-                return state;
-    }
+function Reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    default:
+      return state;
+  }
 }
-const InitialState={user:null};
+const InitialState = { user: null };
 const AuthContextProvider = ({ children }) => {
-    const [state, dispatch]=useReducer(Reducer, InitialState)
-    function LOGIN(data){
-        dispatch({type:"LOGIN", payload:data})
+  const [state, dispatch] = useReducer(Reducer, InitialState);
+  function LOGIN(data) {
+    dispatch({ type: "LOGIN", payload: data });
+  }
+  function LOGOUT() {
+    dispatch({ type: "LOGOUT" });
+  }
+  async function getUserData() {
+    try {
+      const response = await axios.get("http://localhost:3001/api/v1/auth/validate-token", {
+        withCredentials: true,
+      });
+      if (response.data.success) {
+        LOGIN(response.data.user);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    function LOGOUT(){
-        dispatch({type:"LOGOUT"})
+  }
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token) {
+      getUserData(token);
     }
-    async function getUserData(token){
-        try{
-            const response = {
-                data: { success: true, userData:{name:'Awdiz', email:"awdiz@gmail.com"}},
-              }
-            if (response.data.success) {
-                LOGIN(response.data.userData)
-        }}catch(error){
-            console.log(error)
-        }
-    }
-    useEffect(() => {
-        const token = JSON.parse(localStorage.getItem("token"))
-        if (token) {
-            getUserData(token)
-        }
-    }, [])
-    // reducer function hai, initialstate object h
-  return <AuthContext.Provider value={{state, LOGIN, LOGOUT}}>{children}</AuthContext.Provider>;
+  }, []);
+  // reducer function hai, initialstate object h
+
+  return (
+    <AuthContext.Provider value={{ state, LOGIN, LOGOUT }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 export default AuthContextProvider;
